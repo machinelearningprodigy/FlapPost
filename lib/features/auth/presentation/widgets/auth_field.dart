2d43df6faccdeaf7477bbce003dc2fd05cd59f0c
Rahul Mishra <avaantivirus2021@gmail.com>
2025-02-13 12:46:01 +0530

    Refreshed: auth_field.dart

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -22,7 +22,7 @@ class AuthField extends StatelessWidget {
       ),
       validator: (value){
         if(value!.isEmpty){
-          return "$hintText try to missing!";
+          return "$hintText try  missing!";
         }
         return null;
       },
