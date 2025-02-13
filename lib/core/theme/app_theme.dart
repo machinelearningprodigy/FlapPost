d8d6da26a417826a81029caff7f1b6bc4bdb6d3b
Rahul Mishra <avaantivirus2021@gmail.com>
2025-02-13 11:04:36 +0530

    Added lib/core/theme/app_theme.dart

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:blogging_app/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+class AppTheme {
+
+  static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder( //_border is a function that returns an OutlineInputBorder
+        borderSide: BorderSide(
+          color: color,
+          width: 3, 
+        ),
+        borderRadius: BorderRadius.circular(10),
+      );
+ 
+
+
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor : AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(25),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient2),
+    ),
+  );
+}
