c6b545ef3468d53caaf6c3f09a64478eb226b86e
Rahul Mishra <avaantivirus2021@gmail.com>
2025-02-13 11:04:37 +0530

    Added lib/features/auth/presentation/pages/signup_page.dart

---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,77 @@
+import 'package:blogging_app/core/theme/app_colors.dart';
+import 'package:blogging_app/features/auth/presentation/pages/auth_signup_button.dart';
+import 'package:blogging_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        key : formKey,
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Sign Up', 
+                style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+              AuthField(hintText: 'Name', controller: nameController),
+            const SizedBox(height: 15),
+               AuthField(hintText: 'Email', controller: emailController,),
+            const SizedBox(height: 15),
+               AuthField(hintText: 'Password', controller: passwordController, isObscureText: true,),
+            const SizedBox(height: 20),
+              const AuthSignupButton(buttonText: 'Sign Up',),
+            const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: "Already have an account? ",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: ' Sign In',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: AppPallete.gradient2,
+                          fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
