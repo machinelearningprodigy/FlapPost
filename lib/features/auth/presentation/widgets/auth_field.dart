1588691410d455ec08ea00384fe81bc262033f94
Rahul Mishra <avaantivirus2021@gmail.com>
2025-02-13 17:04:33 +0530

    fix: update validation message in AuthField widget

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -22,7 +22,7 @@ class AuthField extends StatelessWidget {
       ),
       validator: (value){
         if(value!.isEmpty){
-          return "$hintText is missing!";
+          return "$hintText final 2missing!";
         }
         return null;
       },
